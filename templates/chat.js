46e28e1bbabf2b67ce345746ad7d2bf660f8873f
Xiaomao Chen <xiaomao5@live.com>
2012-11-20 12:38:33 -0500

    Made interface work.

--- templates/chat.js
+++ templates/chat.js
@@ -21,14 +21,26 @@ $.fn.getCaret = function() {
 	return this[0].selectionStart;
 };
 
+function say(user, message) {
+	var content = '<div class="message"><span class="username">' + user + ': </span><span class="msgcontent">' + message + '</span></div>';
+	$(".content").append(content);
+	$(".content").scrollTop($(".content")[0].scrollHeight);
+}
+
+function sayInput() {
+	say('test', $('.jqte_Content').html());
+	$('.jqte_Content').html('');
+}
+
 $(function() {
 	$(".content").scrollTop($(".content")[0].scrollHeight);
 	$('textarea').jqte();
 	
 	$('.jqte_Content').keydown(function (e) {
 		if (e.keyCode == 10 || e.keyCode == 13) {
-			alert($('.jqte_Content').text());
-			$('.jqte_Content').text('');
+			sayInput();
 		}
 	});
+	
+	$('button').button().click(sayInput);
 });
